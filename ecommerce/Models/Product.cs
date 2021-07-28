d2a040e999d5a15d97cb0814c8ac4e07648c85d4
USBDRIVE <adammclean265@gmail.com>
2021-07-27 17:29:39 -0700

    Created product model

---
+++ ecommerce/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ecommerce.Models
+{
+    /// <summary>
+    /// A saleable product
+    /// </summary>
+    public class Product
+    {
+        [Key] //Make primary key in database
+        public int ProductId{ get; set; }
+        /// <summary>
+        /// consumer facing title of product
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// retail price of the product
+        /// </summary>
+        public double Price { get; set; }
+        /// <summary>
+        /// product category
+        /// </summary>
+        public string category { get; set; }
+    }
+}
