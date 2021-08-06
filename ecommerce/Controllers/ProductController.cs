a47a4177377286d11d8dfa9c5c66375eef4ed192
USBDRIVE <adammclean265@gmail.com>
2021-08-06 12:21:46 -0700

    Edit Product Implemented

--- ecommerce/Controllers/ProductController.cs
+++ ecommerce/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using ecommerce.Data;
 using ecommerce.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,27 +9,26 @@ using System.Threading.Tasks;
 
 namespace ecommerce.Controllers
 {
-       
+
     public class ProductController : Controller
-    
+
     {
         private readonly ProductContext _context;
         public ProductController(ProductContext context)
         {
             _context = context;
         }
-         /// <summary>
+        /// <summary>
         /// Displays a view that lists all products
         /// </summary>
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             //get all products from database
             //List<Product> products = _context.Products.ToList();
-            List<Product> products = 
-                    (from p in _context.Products
-                    select p).ToList();
-
+            List<Product> products =
+                  await (from p in _context.Products
+                         select p).ToListAsync();
             //send list of products to view to be dispalyed
             return View(products);
         }
@@ -38,13 +38,13 @@ namespace ecommerce.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Add(Product p)
+        public async Task<IActionResult> Add(Product p)
         {
             if (ModelState.IsValid)
             {
                 //add to db
                 _context.Products.Add(p);
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
 
 
                 TempData["Message"] = $"{p.ProductId}:{p.Title} was added successfully";
@@ -52,6 +52,34 @@ namespace ecommerce.Controllers
                 return RedirectToAction("Index");
             }
             return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            // get produtct iwth corresponding id
+            Product p =
+             await (from prod in _context.Products
+                    where prod.ProductId == id
+                    select prod).SingleAsync();
+
+
+
+
+            // pass product to view
+
+
+            return View(p);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry(p).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                ViewData["Message"] = "Product updated successfullly";
+            }
+            return View(p);
         }
     }
 }
