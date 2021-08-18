1b79495e6fbb0f4d46b6e0dc70f1ab35ae22614a
USBDRIVE <adammclean265@gmail.com>
2021-08-17 23:02:41 -0700

    Added a register page and register view model

--- ecommerce/Models/UserAccount.cs
+++ ecommerce/Models/UserAccount.cs
@@ -18,16 +18,20 @@ namespace ecommerce.Models
     public class RegisterViewModel
     {
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
         [Compare(nameof(Email))]
         [Required]
+        [Display(Name = "Confirm Email")]
         public string ConfirmEmail { get; set; }
         [Required]
         [DataType(DataType.Password)]
+        [StringLength(120, MinimumLength = 6, ErrorMessage ="Password must be between 6 and 120")]
         public string Password { get; set; }
         [Required]
         [DataType(DataType.Password)]
         [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
         public string ConfirmPassword { get; set; }
         [DataType(DataType.Date)] // time is ignored
         public DateTime? DateOfBirth { get; set; }
